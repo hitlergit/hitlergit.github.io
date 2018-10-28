a06388dbc049c1be0b9691b850a426c4f2f97908
Hitler <a85504@alunos.uminho.pt>
2018-10-28 23:30:46 +0000

    o meu chá

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-        function ajax_get(url, callback) {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function () {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                    console.log('responseText:' + xmlhttp.responseText);
-                    try {
-                        var data = JSON.parse(xmlhttp.responseText);
-                    } catch (err) {
-                        console.log(err.message + " in " + xmlhttp.responseText);
-                        return;
-                    }
-                    callback(data);
-                }
-            };
-
-            xmlhttp.open("GET", url, true);
-            xmlhttp.send();
+function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch (err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
         }
+    };
 
-        var api_url = 'https://api.thedogapi.com/v1/images/search?'
-        api_url += "mime_types=gif"
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
 
-        ajax_get(api_url, function (data) {
-            var html = '<img src="' + data[0]["url"] + '">';
-            document.getElementById("image").innerHTML = html;
-        });
+var api_url = 'https://api.thedogapi.com/v1/images/search?'
+api_url += "mime_types=gif"
+
+ajax_get(api_url, function(data) {
+    var html = '<img src="' + data[0]["url"] + '">';
+    document.getElementById("image").innerHTML = html;
+});
