6124b4ac5f139b0d901dc5375479af69106f04b2
Hitler <a85504@alunos.uminho.pt>
2018-10-29 00:19:25 +0000

    o meu chá

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function ajax_get(url, callback) {
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             console.log('responseText:' + xmlhttp.responseText);
             try {
@@ -20,7 +20,11 @@ function ajax_get(url, callback) {
 var api_url = 'https://api.thedogapi.com/v1/images/search?'
 api_url += "mime_types=gif"
 
-ajax_get(api_url, function(data) {
+ajax_get(api_url, function (data) {
     var html = '<img src="' + data[0]["url"] + '">';
     document.getElementById("image").innerHTML = html;
 });
+
+function cavacoToggle() {
+    document.body.style.backgroundImage = "url('images/cavaco_animated.gif'), url('images/rainbow.gif')";
+}
